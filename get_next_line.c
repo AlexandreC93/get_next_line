522851f7acc4883cd9de59ac7bb0220b4fdec694
Alexandre <71764532+AlexandreC93@users.noreply.github.com>
2022-12-04 04:04:26 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -134,7 +134,7 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = ft_read(fd, buffer);
 	if (!buffer)
